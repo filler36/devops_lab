44ef107de698f00834c01cfb44cf0d7f8f40990f
filler36 <47821378+filler36@users.noreply.github.com>
2019-02-27 22:10:07 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -3,8 +3,7 @@ y = int(input())
 z = int(input())
 n = int(input())
 
-for i in range(0, x + 1):
-    for j in range(0, y + 1):
-        for k in range(0, z + 1):
-            if i + j + k != n:
-                print([i, j, k])
+print([[i, j, k] for i in range(x + 1)
+      for j in range(y + 1)
+      for k in range(z + 1)
+      if ((i + j + k) != n)])
