754031f4bf02400a2352092749da2a3cf6f81fd6
filler36 <47821378+filler36@users.noreply.github.com>
2019-02-27 22:46:26 +0300

    Update coord.py

--- coord.py
+++ coord.py
@@ -1,11 +1,9 @@
 def calc(x, y, z, n):
-    ar = []
-    for i in range(0, x + 1):
-        for j in range(0, y + 1):
-            for k in range(0, z + 1):
-                if i + j + k != n:
-                    print([i, j, k])
-                    ar.append([i, j, k])
+    ar = ([[i, j, k] for i in range(x + 1)
+           for j in range(y + 1)
+           for k in range(z + 1)
+           if ((i + j + k) != n)])
+    print(ar)
     return ar
 
 
