2d1d39cb688f5402c19ca5fd7f0ab761518db19c
filler36 <47821378+filler36@users.noreply.github.com>
2019-02-28 12:51:22 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -21,15 +21,26 @@ class Mon:
         timestamp = datetime.datetime.now().strftime(settings.TIME_FORMAT)
         threading.Timer(settings.INTERVAL, self.dump).start()
         self.counter += 1
-        with open('data.json', 'a') as outfile:
-            json.dump({'SNAPSHOT ' + str(self.counter): timestamp,
-                       'CPU_LOAD_%': cpu_load,
-                       'MEM_TOTAL_IN_GB': float("{:.2f}".format(mem_total)),
-                       'MEM_USED_IN_GB': float("{:.2f}".format(mem_used)),
-                       'READ_COUNT': disk_io[0],
-                       'WRITE_COUNT': disk_io[1],
-                       'BYTES_SENT': net_stat[0],
-                       'BYTES_RECEIVED': net_stat[1]}, outfile, indent=1)
+        if settings.OUTPUT == 'json':
+            with open('data.json', 'a') as outfile:
+                json.dump({'SNAPSHOT ' + str(self.counter): timestamp,
+                           'CPU_LOAD_%': cpu_load,
+                           'MEM_TOTAL_IN_GB': float("{:.2f}".format(mem_total)),
+                           'MEM_USED_IN_GB': float("{:.2f}".format(mem_used)),
+                           'READ_COUNT': disk_io[0],
+                           'WRITE_COUNT': disk_io[1],
+                           'BYTES_SENT': net_stat[0],
+                           'BYTES_RECEIVED': net_stat[1]}, outfile, indent=1)
+        elif settings.OUTPUT == 'txt':
+            with open('data.txt', 'a') as outfile:
+                json.dump({'SNAPSHOT ' + str(self.counter): timestamp,
+                           'CPU_LOAD_%': cpu_load,
+                           'MEM_TOTAL_IN_GB': float("{:.2f}".format(mem_total)),
+                           'MEM_USED_IN_GB': float("{:.2f}".format(mem_used)),
+                           'READ_COUNT': disk_io[0],
+                           'WRITE_COUNT': disk_io[1],
+                           'BYTES_SENT': net_stat[0],
+                           'BYTES_RECEIVED': net_stat[1]}, outfile, indent=1)
 
 
 MONITOR = Mon()
